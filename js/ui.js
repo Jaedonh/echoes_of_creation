162d670887adc4f95184e6d84918e3a69a0354e9
Jaedonh <71844986+Jaedonh@users.noreply.github.com>
2025-02-03 14:04:23 -0600

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,4 +2,9 @@ function updateUI() {
     document.getElementById("echoCount").innerText = echoes.toFixed(2);
     document.getElementById("ascendantCount").innerText = ascendantEchoes;
     document.getElementById("echoRate").innerText = echoRate.toFixed(2);
+
+    // Hide Reality Collapse if reset
+    if (echoes === 0) {
+        document.getElementById("collapseSection").style.display = "none";
+    }
 }
